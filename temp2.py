755471a1e8075eb2e7cf31c156f103a30fb9f381
Quincy Jia <jiaq003@gmail.com>
2020-04-30 23:20:07 -0400

    initializing repository

---
+++ temp2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#求1+2+3+...+100的值
+
+
+
+#依次求[2,4,6,7,8]的平方值
+def f(x):
+    return x*x
+
+print(list(map(f,[2,4,6,7,8]))) #[4, 16, 36, 49, 64]
